c3c843720733f076a08210d3ff8837c2379b88dc
ZaharDanMaster <zahar.kuleshov@mail.ru>
2023-04-12 19:15:47 +0300

    a1

--- wk1/wk1.cpp
+++ wk1/wk1.cpp
@@ -1,5 +1,17 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+void backdoor()
+{
+    int a = 5;
+
+    for (int i = 0; i < a; ++i)
+    {
+        cout << "you noob)";
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
