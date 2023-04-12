ef2379586c064250139a6628d65c14a235d8b5d6
ZaharDanMaster <zahar.kuleshov@mail.ru>
2023-04-12 19:16:33 +0300

    a2

--- wk1/wk1.cpp
+++ wk1/wk1.cpp
@@ -12,6 +12,16 @@ void backdoor()
     }
 }
 
+void doorback()
+{
+    int a = 10;
+
+    for (int i = 0; i < a; ++i)
+    {
+        cout << "you fuck)";
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
